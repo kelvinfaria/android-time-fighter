d3684243f4cdd81e7b4f58813f77dbaf4a1aecd7
Kelvin Faria <kelvin.faria@picpay.com>
2021-02-21 16:26:56 -0300

    Increment score method created

--- app/src/main/java/com/kelvinfaria/timefighter/MainActivity.kt
+++ app/src/main/java/com/kelvinfaria/timefighter/MainActivity.kt
@@ -8,10 +8,20 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.tapMeButton.setOnClickListener {
+            incrementScore()
+        }
+    }
+
+    private fun incrementScore() {
+        score++
+        binding.userScoreTextView.text = getString(R.string.user_score_text, score)
     }
 }
